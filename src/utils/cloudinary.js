35fe4afd74cb9e65850acd58e870ad626a61ec17
swarraaa <vikasgouda4@gmail.com>
2024-07-08 14:47:07 +0530

    cloudinary.js in utils, multer.js in middleware added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      // file path not present
+      console.error("FilePath not present");
+      throw err;
+    }
+
+    const uploadResult = await cloudinary.uploader
+      .upload(localFilePath, {
+        resource_type: "auto",
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    console.log("File uploaded on cloudinary", uploadResult.url);
+
+    return uploadResult;
+  } catch (error) {
+    // remove the locally saved temp file as the upload operation failed
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
